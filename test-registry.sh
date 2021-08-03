d6c1c8d4287a7e4ad181cf9ca42c0a252b32f31f
Tadej Janež <tadej.j@nez.si>
2021-08-03 22:13:15 +0200

    Uncomment registry.RegisterEntity test
    It is working again in the latest version of Oasis App.

--- test-registry.sh
+++ test-registry.sh
@@ -3,22 +3,17 @@
 source common.sh
 
 test_registry() {
+    TXN_FILE="$TXNS_DIR/tx_entity_register.json"
+    $OASIS_NODE registry entity gen_register \
+      "${TX_FLAGS[@]}" \
+      --transaction.file $TXN_FILE \
+      --transaction.nonce $ENTITY1_NONCE \
+      --transaction.fee.gas 1000 \
+      --transaction.fee.amount 2000
 
-    # NOTE: This appears to be broken in the newest version of the Oasis App: 2.1.0:
-    # + oasis-node-21.2.7 registry entity gen_register --genesis.file testnet/genesis.json --signer.dir entity-0/ --signer.backend plugin --signer.plugin.name ledger --signer.plugin.path '/home/tadej/Apps/Oasis Core Ledger/oasis_core_ledger_1.2.0_linux_amd64/ledger-signer' --signer.plugin.config wallet_id:dba676,index:0 --transaction.file testnet/transactions/tx_entity_register.json --transaction.nonce 25 --transaction.fee.gas 1000 --transaction.fee.amount 2000
-    # ts=2021-07-09T16:19:55.607272785Z level=error module=cmd/registry/entity caller=entity.go:297 msg="failed to sign entity descriptor" err="signature/signer/plugin: failed to sign: ledger: failed to sign message: ledger/oasis: failed to sign: Unrecognized error code"
-    #
-    # TXN_FILE="$TXNS_DIR/tx_entity_register.json"
-    # $OASIS_NODE registry entity gen_register \
-    #   "${TX_FLAGS[@]}" \
-    #   --transaction.file $TXN_FILE \
-    #   --transaction.nonce $ENTITY1_NONCE \
-    #   --transaction.fee.gas 1000 \
-    #   --transaction.fee.amount 2000
-
-    # ENTITY1_NONCE=$((ENTITY1_NONCE + 1))
-    # show_tx $TXN_FILE
-    # submit_tx $TXN_FILE
+    ENTITY1_NONCE=$((ENTITY1_NONCE + 1))
+    show_tx $TXN_FILE
+    submit_tx $TXN_FILE
 
     # NOTE: This appears to be broken in the newest and the current latest version
     # of Oasis App published on Ledger Live: 2.1.0 and 1.8.2:
